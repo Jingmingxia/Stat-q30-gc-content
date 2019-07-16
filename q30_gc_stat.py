674584d2546ae0f5f245e0006176e45be12744b3
Jingmingxia <41792282+Jingmingxia@users.noreply.github.com>
2019-07-16 18:06:48 +0800

    Update q30_gc_stat.py

--- q30_gc_stat.py
+++ q30_gc_stat.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 #encoding: utf-8
-
 import sys,os
+
 ################################################
 # The script is to stat the q20, q30, gc_content and output fasta 
 # Usage: python $0 input_fq output_fa
 ###############################################
+
 def stat_qual(qstr):
     q20 = 0
     q30 = 0
@@ -78,17 +79,17 @@ gcp = round(float(gc)*100/float(dna),2)
 q30 = round(float(q30_count)*100/float(dna),2)
 q20 = round(float(q20_count)*100/float(dna),2)
 print("""Base\tNumber\tPercentage
-A\t{0:,}\t{1:,}
-T\t{2:,}\t{3:,}
-C\t{4:,}\t{5:,}
-G\t{6:,}\t{7:,}
-N\t{8:,}\t{9:,}
-GC\t{10:,}\t{11:,}
-Total\t{12:,}\t{13:,}
+A:\t{0}\t{1}
+T:\t{2}\t{3}
+C\t{4}\t{5}
+G\t{6}\t{7}
+N\t{8}\t{9}
+GC\t{10}\t{11}
+Total\t{12}\t{13}
 """.format(a,ap,t,tp,c,cp,g,gp,n,np,gc,gcp,dna,100.00))
 print("""Total_reads:{0}\n
 Quality_type\tBases\tPercentage
-q20:{1}\t{2}
-q30:{3}\t{4}
+q20:\t{1}\t{2}
+q30:\t{3}\t{4}
 """.format(reads,q20_count,q20,q30_count,q30))
 
